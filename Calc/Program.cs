3a542f702487ccd48e5dac4fec87a6b3a3b6637e
Liam <5226661@A027712>
2021-06-17 12:13:56 +0100

    7

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -12,6 +12,7 @@ namespace Calc
         {
             double inum1 = 0;
             double inum2 = 0;
+            int inum3 = 0;
             double ianswer = 0;
             char cOption;
             bool now = true;
@@ -20,31 +21,57 @@ namespace Calc
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine("Menu\nA is for Addition\nS is for Subtraction\nM is for Multiplication\nD is for Devision\nL is for last answer\nE is for Exit\nPlease choose menu option for operator");
+                Console.WriteLine("Menu\nA is for Addition\nS is for Subtraction\nM is for Multiplication\nD is for Division\nP is for Square\nR is for Square root\nL is for last answer\nE is for Exit\nPlease choose menu option for operator");
 
                 cOption = char.Parse(Console.ReadLine());
                 cOption = char.ToUpper(cOption);
 
-                Console.WriteLine("Please enter first number");
-                inum1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter second number");
-                inum2 = double.Parse(Console.ReadLine());
-
                 switch (cOption)
                 {
                     case 'A':
+                        Console.WriteLine("Please enter first number");
+                        inum1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter second number");
+                        inum2 = double.Parse(Console.ReadLine());
                         ianswer = inum1 + inum2;
                         break;
                     case 'S':
+                        Console.WriteLine("Please enter first number");
+                        inum1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter second number");
+                        inum2 = double.Parse(Console.ReadLine());
                         ianswer = inum1 - inum2;
                         break;
                     case 'M':
+                        Console.WriteLine("Please enter first number");
+                        inum1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter second number");
+                        inum2 = double.Parse(Console.ReadLine());
                         ianswer = inum1 * inum2;
                         break;
                     case 'D':
+                        Console.WriteLine("Please enter first number");
+                        inum1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter second number");
+                        inum2 = double.Parse(Console.ReadLine());
                         ianswer = inum1 / inum2;
                         break;
+                    case 'P':
+                        Console.WriteLine("What number do you want to Square?");
+                        inum3 = int.Parse(Console.ReadLine());
+                        ianswer = inum3 * inum3;
+                        break;
+                    case 'R':
+                        Console.WriteLine("What number do you want to Square root?");
+                        inum3 = int.Parse(Console.ReadLine());
+                        ianswer = Math.Sqrt(inum3);
+                        break;
                     case 'L':
+                        Console.WriteLine("What Calculation number would you like to see? ");
+                        inum3 = int.Parse(Console.ReadLine());
+                        List<double> mem = new List<double> { };
+                        mem.Add(ianswer);
+                        Console.WriteLine(mem[inum3]);
                         break;
                     case 'E':
                         now = false;
@@ -53,7 +80,6 @@ namespace Calc
                         Console.WriteLine("Error try again");
                         break;
                 }
-
                 Console.WriteLine("\nThe answer is " + ianswer + "\n");
 
                 counter = counter + 1;
